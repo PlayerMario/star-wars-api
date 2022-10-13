98eb0fed9e9ac53a2b861d30cac0e0f186749f86
Admin <hidalgo.najos22@triana.salesianos.edu>
2022-10-13 14:25:56 +0200

    Cambio de ruta para pagenot found, y film-list-component.html actualizado

--- starwars-api/src/app/app-routing.module.ts
+++ starwars-api/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmListComponent } from './components/film/film-list.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { PeopleComponent } from './components/people/people.component';
+import { PortadaComponent } from './components/portada/portada.component';
+import { StarshipsComponent } from './components/starships/starships.component';
 
 const routes: Routes = [
   { path: 'inicio', component: PortadaComponent },
-  { path: 'films', component: FilmsComponent },
-  { path: 'people', component: PeopleListComponent },
+  { path: 'films', component: FilmListComponent },
+  { path: 'people', component: PeopleComponent },
   { path: 'planets', component: PlanetsComponent },
   { path: 'species', component: SpeciesComponent },
   { path: 'starships', component: StarshipsComponent },
   { path: 'vehicles', component: VehiclesComponent },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PagenotfoundComponent }
 ];
 
 
