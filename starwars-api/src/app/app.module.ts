c6987f5883e6ac771f07bb2db2aedaebc7c73dc9
Mario Ruiz López <ruiz.lomar22@triana.salesianos.edu>
2022-10-13 13:05:52 +0200

    Modificación app module, routing y component

--- starwars-api/src/app/app.module.ts
+++ starwars-api/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialImportsModule } from './modules/material-imports.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,8 +13,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialImportsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
