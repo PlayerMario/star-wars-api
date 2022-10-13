fc5b144be1b7fc610da527c611d91f114de33fcb
Mario Ruiz López <ruiz.lomar22@triana.salesianos.edu>
2022-10-13 13:22:03 +0200

    Creación de portada

--- starwars-api/src/app/app.module.ts
+++ starwars-api/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PortadaComponent } from './components/portada/portada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule,
