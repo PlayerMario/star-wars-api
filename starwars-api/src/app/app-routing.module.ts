e26c0d5b156e8b2de0571774e5b0cd79b8e39b80
Mario Ruiz López <ruiz.lomar22@triana.salesianos.edu>
2022-10-14 09:06:40 +0200

    Solución fallos rutas

--- starwars-api/src/app/app-routing.module.ts
+++ starwars-api/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.co
 import { PeopleComponent } from './components/people/people.component';
 import { PortadaComponent } from './components/portada/portada.component';
 import { StarshipsComponent } from './components/starships/starships.component';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
 
 const routes: Routes = [
   { path: 'inicio', component: PortadaComponent },
   { path: 'films', component: FilmListComponent },
   { path: 'people', component: PeopleComponent },
-  { path: 'planets', component: PlanetsComponent },
-  { path: 'species', component: SpeciesComponent },
+  /*{ path: 'planets', component: PlanetsComponent },
+  { path: 'species', component: SpeciesComponent },*/
   { path: 'starships', component: StarshipsComponent },
   { path: 'vehicles', component: VehiclesComponent },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
